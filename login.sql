8c86dd6610384bcf6dc43661d026f82481e5d179
Yoonseo KIM <61778930+ottl-seo@users.noreply.github.com>
2021-07-01 21:31:34 +0900

    Update login.sql

--- login.sql
+++ login.sql
@@ -1,6 +1,6 @@
-create table User (
-    seq_number bigint AUTO_INCREMENT primary key,
-    name varchar(100),
-    id varchar(255),
-    password varchar(255)
+create table user_info (
+    code bigint primary key,
+    email varchar(100),
+    password varchar(255),
+    auth varchar(100)
     );
