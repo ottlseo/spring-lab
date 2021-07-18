690e2c307946f3430888946de429607264562392
Yoonseo KIM <61778930+ottl-seo@users.noreply.github.com>
2021-07-18 19:10:03 +0900

    일정 기간동안의 커밋 수 가져오기

--- GITHUB-API-사용하기/GithubInfoController.java
+++ GITHUB-API-사용하기/GithubInfoController.java
@@ -1,45 +1,30 @@
 package ewhacodic.demo.controller;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
 import ewhacodic.demo.dto.GithubInfoDto;
+import ewhacodic.demo.service.GithubInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import com.google.gson.Gson;
-import com.fasterxml.jackson.*;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.json.simple.JSONObject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import java.util.List;
-
+@RequiredArgsConstructor
+@Controller
 public class GithubInfoController {
-    // 클라이언트에 url 매핑이 되어야 하고
-    @RequestMapping(value = "/url", method = RequestMethod.POST, produces = "application/json; charset=utf8")
+    private final GithubInfoService githubInfoService;
 
-    // 구글의 json paser 라이브러리
-    Gson gson = new Gson();
+    //테스트용
+    @GetMapping(value = "/api/commit_count_test")
+    public long commit_count_test(GithubInfoDto githubInfoDto) {
 
-    // jsonPaserPser 클래스 객체를 만들고 해당 객체에
-    JsonParser jparser = new JsonParser();
+        //임의로 상황 설정해줌
+        githubInfoDto.setUserName("ottl-seo");
+        githubInfoDto.setRepoName("Algorithm");
+        githubInfoDto.setStartDate("2021-07-01T00:00:00Z");
+            //CurrentDate는 자동 생성
 
-    // param의 id 오브젝트 -> 문자열 파싱 -> jsonElement 파싱
-    JsonElement elementId = jparser.parse(param.get("id").toString());
-    // JsonElement -> List<String>으로 파싱
-    List<String> idList = gson.fromJson(elementId, (new TypeToken<List<String>>() {  }).getType());
+        Long commits = githubInfoService.CommitCount(githubInfoDto);
+        return commits;
+    }
 }
-
-public GithubInfoDto test(){
-       /* GithubInfoDto test = new GithubInfoDto(5);
-        return  test;
-        ObjectMapper objectMapper =new ObjectMapper();
-
-        String url ="https://api.github.com/repos/ejhee1/FEF/stats/punch_card
-        ?since=2021-02-09T00:00:00Z&until=2021-02-10T23:59:59Z?author=ejhee1";
-
-        GithubInfoDto t = objectMapper.readValue(url, GithubInfoDto.class);
-*/
-        }
