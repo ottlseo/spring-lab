0e4cd86150387ffdd7407b313bca2d4d193a95ee
Yoonseo KIM <61778930+ottl-seo@users.noreply.github.com>
2021-07-28 19:44:21 +0900

    Feat: fix cors error

--- G메일-인증번호-전송하기/MailController.java
+++ G메일-인증번호-전송하기/MailController.java
@@ -1,13 +1,12 @@
-package ewhacodic.demo.controller;
+package codic.ewhain.Controller;
 
-import ewhacodic.demo.util.MailUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
@@ -16,17 +15,20 @@ import java.util.Random;
 
 
 @RequiredArgsConstructor
-@Controller
-public class MailController {
+@RestController
+public class EmailController {
 
-    @RequestMapping(value = "/checkMail", method = {RequestMethod.GET, RequestMethod.POST})
-    public Map<String, Object> SendMail(String mail, HttpSession session) {
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value="/CheckMail", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public Map<String, Object> SendMail(@RequestBody String id, HttpSession session) throws Exception {
 
         Map<String, Object> map = new HashMap<>();
         Random random = new Random();
         JavaMailSender javaMailSender = new JavaMailSenderImpl();
         //MailUtils mailUtils = new MailUtils(javaMailSender); //내가 추가한 코드
         String key = "";
+        String mail = id+"@ewhain.net";
 
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(mail); // 스크립트에서 보낸 메일을 받을 사용자 이메일 주소
@@ -39,7 +41,7 @@ public class MailController {
         key += numIndex;
         message.setSubject("이화코딕 회원가입을 위한 인증번호 전송");
         message.setText("인증 번호 : " + key);
-        message.setFrom("EWHA CODIC");
+        message.setFrom("CODIC");
         javaMailSender.send(message);
         map.put("key", key);
         return map;
