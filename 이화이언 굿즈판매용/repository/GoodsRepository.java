941962fb0f64e5a7a8995a06ac687e370b3d3bc4
Yoonseo KIM <61778930+ottl-seo@users.noreply.github.com>
2021-08-31 02:08:31 +0900

    static/non-static 문제 해결 필요
    repository를 static으로 만들순 X
    - save(): JpaRepository 함수라 바꾸지 못함
    - findByOrderId(): 바꾸면 body를 추가해줘야함(인터페이스라서)
    
    JPA 더 공부하기

--- 이화이언 굿즈판매용/repository/GoodsRepository.java
+++ 이화이언 굿즈판매용/repository/GoodsRepository.java
@@ -11,5 +11,4 @@ import java.util.Optional;
 public interface GoodsRepository extends JpaRepository<Goods, String> {
     // save()는 JPARepository 상속받았기 때문에 오버라이딩
     Optional<Goods> findByOrderId(String orderId); // 고유번호로 주문 조회
-    //List<Goods> find
 }
