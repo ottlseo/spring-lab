7d1b4d2184c362217a03affcdf2f5340e7f0d399
Yoonseo KIM <dotsi@ewhain.net>
2021-11-13 23:17:35 +0900

    fixed untracked files

---
+++ src/test/java/efub/insta/InstaApplicationTests.java
@@ -0,0 +1,13 @@
+package efub.insta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
